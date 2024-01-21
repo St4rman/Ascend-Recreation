bac81504273dc0d84a558f8e0ddb2292728c38e1
Eddu <55552484+St4rman@users.noreply.github.com>
2024-01-21 18:42:46 +0000

    Point of Exit
    Calculated the point of exit of the cieling above

--- Assets/Scripts/player/Abilities/ascend.cs
+++ Assets/Scripts/player/Abilities/ascend.cs
@@ -6,8 +6,11 @@ public class ascend : MonoBehaviour
 {
     [Header("References")]
     public GameObject PlayerRef;
-    public bool ascendIsActivated;
+    bool ascendIsActivated  = true;
+    public bool canAscend;
+    public LayerMask IgnoreLayer;
     private Collider objectColl;
+    private Vector3 pointOfExitOnSurface;
     void Start()
     {
         
@@ -19,6 +22,11 @@ public class ascend : MonoBehaviour
         if(ascendIsActivated){
             fireRaycast();
         }
+        if(ascendIsActivated && canAscend){
+            if(Input.GetKeyDown(KeyCode.E)){
+                StartCoroutine(StartAscend(pointOfExitOnSurface));
+            }
+        }
     }
 
     private void fireRaycast(){
@@ -29,19 +37,46 @@ public class ascend : MonoBehaviour
 
         RaycastHit hit;
         
-        if(Physics.Raycast(ascendRay.origin, ascendRay.direction, out hit, Mathf.Infinity)){
+        if(Physics.Raycast(ascendRay.origin, ascendRay.direction, out hit, Mathf.Infinity, ~IgnoreLayer)){
 
             Debug.DrawRay(ascendRay.origin, ascendRay.direction * hit.distance, Color.green);
             
             if(hit.collider != null){
+
                 objectColl = hit.collider;
-                Debug.Log("checking max" + objectColl.bounds.max);
+
+                //we're getting point of exit manually here
+                Vector3 ExitPoint = new Vector3(hit.point.x, hit.point.y + 2* objectColl.bounds.extents.y, hit.point.z);
+                
+                checkIfObstructed(ExitPoint);
             }
 
+        } else { 
+            canAscend = false;
         }
     }
 
-    private void checkIfObstructed(){
+    private void checkIfObstructed(Vector3 pointofExit){
 
+        Ray ray = new Ray(pointofExit, transform.up);
+        RaycastHit oHit;
+
+        if(Physics.Raycast(ray.origin, ray.direction, out oHit, Mathf.Infinity, ~IgnoreLayer)){
+
+            if(oHit.collider != null){ //adn and has enough place for player to stand
+                canAscend = false;
+            }
+
+        } else {
+            //if there is enough space for player model
+            pointOfExitOnSurface = pointofExit;
+            canAscend = true;
+        }
+    }
+
+    IEnumerator StartAscend(Vector3 AscendToPoint){
+        Debug.Log(AscendToPoint);
+        PlayerRef.GetCompo
+        yield return null;
     }
 }
